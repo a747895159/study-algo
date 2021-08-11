6e7697d489273be320ace27f7098f3db8ad1ef4d
zhoubin <zhoubin2@yonghui.cn>
2021-08-11 14:56:21 +0800

    项目搭建

---
+++ src/main/java/com/person/algo/A.java
@@ -0,0 +1,13 @@
+package com.person.algo;
+
+/**
+ * @Desc:
+ * @Author: ZhouBin
+ * @Date: 2021/8/11
+ */
+public class A {
+
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
+}
