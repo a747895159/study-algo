fe4c3447e400c2a72a4866b73adaea22a21faa58
zhoubin <zhoubin2@yonghui.cn>
2024-05-28 15:59:46 +0800

    增加依赖

--- src/main/java/com/person/algo/螺旋矩阵/LXTest0001.java
+++ src/main/java/com/person/algo/螺旋矩阵/LXTest0001.java
@@ -13,51 +13,81 @@ import com.alibaba.fastjson.JSONObject;
 public class LXTest0001 {
 
     public static void main(String[] args) {
-        int n = 3;
-        exec(n);
+        int n = 1;
+        int[][] arr = exec(n);
+
+        exec2(arr);
     }
 
-    public static void exec(int n) {
-        int[][] nums = new int[n][n];
-        int startX = 0, startY = 0;  // 每一圈的起始点
+    public static int[][] exec(int n) {
+        int[][] arr = new int[n][n];
+        int sx = 0;
+        int sy = 0;
         int offset = 1;
-        int count = 1;  // 矩阵中需要填写的数字
-        int loop = 1; // 记录当前的圈数
-        int i, j; // j 代表列, i 代表行;
-
-        while (loop <= n / 2) {
-
-            // 顶部
-            // 左闭右开，所以判断循环结束时， j 不能等于 n - offset
-            for (j = startY; j < n - offset; j++) {
-                nums[startX][j] = count++;
+        int num = 1;
+        int loop = n / 2;
+        while (loop > 0) {
+            for (sy = offset - 1; sy < n - offset; sy++) {
+                arr[sx][sy] = num++;
             }
-
-            // 右列
-            // 左闭右开，所以判断循环结束时， i 不能等于 n - offset
-            for (i = startX; i < n - offset; i++) {
-                nums[i][j] = count++;
+            for (sx = offset - 1; sx < n - offset; sx++) {
+                arr[sx][sy] = num++;
             }
-
-            // 底部
-            // 左闭右开，所以判断循环结束时， j != startY
-            for (; j > startY; j--) {
-                nums[i][j] = count++;
+            for (; sy > offset - 1; sy--) {
+                arr[sx][sy] = num++;
             }
-
-            // 左列
-            // 左闭右开，所以判断循环结束时， i != startX
-            for (; i > startX; i--) {
-                nums[i][j] = count++;
+            for (; sx > offset - 1; sx--) {
+                arr[sx][sy] = num++;
             }
-            startX++;
-            startY++;
             offset++;
-            loop++;
+            loop--;
+            sx++;
+            sy++;
         }
-        if (n % 2 == 1) { // n 为奇数时，单独处理矩阵中心的值
-            nums[startX][startY] = count;
+        if (n % 2 != 0) {
+            arr[sx][sy] = num;
         }
-        System.out.println(JSONObject.toJSON(nums));
+        System.out.println(JSONObject.toJSON(arr));
+
+        return arr;
+    }
+
+    public static int[] exec2(int[][] nums) {
+        if (nums == null || nums.length == 0 || nums[0].length == 0 || nums.length!=nums[0].length) {
+            return new int[0];
+        }
+        int n = nums.length;
+        //遍历二维数组将[[1,2,3,4],[12,13,14,5],[11,16,15,6],[10,9,8,7]] 输出 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+        int[] arr = new int[n * n];
+        int sx = 0;
+        int sy = 0;
+        int loop = n / 2;
+        int index = 0;
+        int offset = 1;
+        while (loop > 0) {
+            for (; sy < n - offset; sy++) {
+                arr[index++] = nums[sx][sy];
+            }
+            for (; sx < n - offset; sx++) {
+                arr[index++] = nums[sx][sy];
+            }
+            for (; sy > offset - 1; sy--) {
+                arr[index++] = nums[sx][sy];
+            }
+            for (; sx > offset - 1; sx--) {
+                arr[index++] = nums[sx][sy];
+            }
+            offset++;
+            loop--;
+            sx++;
+            sy++;
+        }
+
+        if (n % 2 != 0) {
+            arr[index] = nums[sx][sy];
+        }
+        System.out.println(JSONObject.toJSON(arr));
+
+        return arr;
     }
 }
